ab97a60144c5a77c4434ac4ef4c7f1ca3f24a80e
anomalisuki <davied2603@gmail.com>
2026-08-01 18:59:20 +0700

    Update maintenance: Buka/Bersihkan semua maintenance

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 // ─── PENGATURAN MAINTENANCE AMBA TV ───
 const ALL_MAINTENANCE = false;
-const MAINTENANCE_CHANNELS = ['mentari-tv'];
+const MAINTENANCE_CHANNELS = [];
 // ───────────────────────────────────────
 
 export default async function middleware(request) {
